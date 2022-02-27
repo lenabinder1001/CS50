9ee5c01b6b53148c1f92522e28fea6aa7a5f2f43
lenabinder1001 <96173519+lenabinder1001@users.noreply.github.com>
2022-02-27 09:27:53 +0100

    added code from week 1

---
+++ week_1/mario-more/mario.c
@@ -0,0 +1,40 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Get positive integer between 1 and 8 (inclusive) from user
+    int stored;
+
+    do
+    {
+        stored = get_int("Height: ");
+    }
+    while (stored < 1 || stored > 8);
+
+    for (int i = 0; i < stored; i++)
+    {
+        // For-Loop to add spaces
+        for (int k = stored - i; k > 1; k--)
+        {
+            printf(" ");
+        }
+
+        // For-Loop to add hashes
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+
+        // Add space between blocks
+        printf("  ");
+
+        // For-Loop to add hashes
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+
+}
